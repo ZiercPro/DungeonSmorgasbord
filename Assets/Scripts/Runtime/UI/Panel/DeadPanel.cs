285abc8b7c69ce5d9613e9e4010e1e9c3b166203
Zierc <3042957340@qq.com>
2024-03-18 20:56:48 +0800

    add deadpanel
    添加死亡页面

---
+++ Assets/Scripts/Runtime/UI/Panel/DeadPanel.cs
@@ -0,0 +1,9 @@
+
+public class DeadPanel : BasePanel
+{
+    public readonly static string path = "Prefabs/UI/Panel/DeadPanel";
+
+    public DeadPanel(UIType uiType) : base(new UIType(path)) { }
+    
+    
+}
